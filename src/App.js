207a944cdc0878d78c1fe84340fdd0c19398cef4
Topaz Freizeit <topazfreizeit@gmail.com>
2023-05-11 14:50:02 +0300

    added video

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import videoFile from './1-example.mp4';
 
 function App() {
+  const videoRef = useRef(null);
+
+  const playVideo = () => {
+    videoRef.current.play();
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Team 59 - Salesforce winner</h1>
+      <div className="button-container">
+      <button className="button" onClick={playVideo}>
+          Play Video
+        </button>
+        <button className="button">Button 2</button>
+        <button className="button">Button 3</button>
+      </div>
+      <video
+        ref={videoRef}
+        className="video"
+        src={videoFile}
+      />
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
